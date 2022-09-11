dcc580f07a1c0994c444987bf4a9d19f1471e742
shahidshaik92 <shahidshaik921@gmail.com>
2022-09-11 16:59:31 +0530

    array example

---
+++ src/com/example/CountExample.java
@@ -0,0 +1,27 @@
+package com.example;
+
+public class CountExample {
+	public static void main(String[] args) {
+		int array[] = { 1, 2, 3, -1, -2, -3 };
+		System.out.println("Given positive number");
+		int i, positive = 0;
+		int negative = 0;
+		for (i = 0; i < array.length; i++) {
+			if (array[i] > 0) {
+				positive = positive + 1;
+
+			}
+		}
+		System.out.println(positive);
+		System.out.println("Given negative number");
+		for (int i1 = 0; i1 < array.length; i1++) {
+			if (array[i1] < 0) {
+				negative = negative + 1;
+
+			}
+
+		}
+		System.out.println(negative);
+	}
+
+}
--- src/com/example/PositiveNegativeExample.java
+++ src/com/example/PositiveNegativeExample.java
@@ -2,23 +2,24 @@ package com.example;
 
 public class PositiveNegativeExample {
 	public static void main(String[] args) {
-		int array[] = { 1, 2, 3, 0, -1, -4, -6 };
-		System.out.println("Given positive numbers");
-		int i, positive = 0, negative = 0;
-		for (i = 0; i < array.length; i++) {
+		int array[] = { 1, 2, 3, -1, -2, -3,0 };
+		System.out.println("given positive numbers");
+		for (int i = 0; i < array.length; i++) {
 			if (array[i] > 0) {
-				positive = positive + 1;
+				System.out.println(array[i]);
 			}
-		}
-		System.out.println(positive);
-		System.out.println("Given negative numbers");
-		for (i = 0; i < array.length; i++) {
-			if (array[i] < 0) {
-				negative = negative + 1;
-			}
-		}
-		System.out.println(negative);
 
+			
+		}
+		System.out.println("given negative number");
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] <=0) {
+				System.out.println(array[i]);
+			
+				
+			}
+		
 	}
 
+}
 }
